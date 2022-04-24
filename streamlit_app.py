cd0167591030bf92191b00fd52ec1ade8d85054e
surajraj85 <surajraj85@gmail.com>
2022-04-24 11:56:16 -0400

    Update streamlit_app.py
    URLError

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header('Breakfast Favorites')
